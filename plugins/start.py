d47124b4f4b58710f435f6cf2411069fbd332850
Lx 0980 <93909675+alex0980@users.noreply.github.com>
2023-04-29 23:35:29 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -18,8 +18,8 @@ async def start(bot, cmd):
           reply_markup = InlineKeyboardMarkup(
                 [
                     [
-                        InlineKeyboardButton("Help", callback_data="help_data"),
-                        InlineKeyboardButton("About", callback_data="about_data"),
+                        InlineKeyboardButton("Help", callback_data="helpbutton_data"),  
+                        InlineKeyboardButton("About", callback_data="about_data")
                     ],[                    
                         InlineKeyboardButton(
                             "Developer", url="https://t.me/Lx0980AI")
@@ -30,3 +30,88 @@ async def start(bot, cmd):
 
 
  
+@Robot.on_callback_query()
+async def button(bot, cmd: CallbackQuery):
+    cb_data = cmd.data
+    if "helpbutton_data" in cb_data:
+        await cmd.message.edit(
+             text = "Help: <b>Buttons</b>",
+             parse_mode = enums.ParseMode.MARKDOWN, 
+             disable_web_page_preview=True, 
+             reply_markup=InlineKeyboardMarkup(
+                 [
+                     [
+                      InlineKeyboardButton("Auto Filter", callback_data="autofilter_data"),
+                      InlineKeyboardButton("Media Clone", callback_data="mediaclone_data"),
+                     ],[
+                      InlineKeyboardButton("Developer", url="https://t.me/Lx0980AI")
+                     ],[
+                      InlineKeyboardButton("🔐 CLOSE", callback_data="close_data")
+                     ]
+ 
+                 ] 
+             ) 
+        )
+    elif "mediaclone_data" in cb_data:
+          await cmd.message.edit(
+               text=Script.MEDIACLONE_TXT,
+               parse_mode = enums.ParseMode.HTML, 
+               disable_web_page_preview=True, 
+               reply_markup=InlineKeyboardMarkup(
+                   [
+                       [                                             
+                        InlineKeyboardButton("⬇️ Back", callback_data="helpbutton_data"),
+                        InlineKeyboardButton("🔐 Close", callback_data="close_data")                                               InlineKeyboardButton("Auto Filter", url="https://t.me/Lx0980AI")
+                       ] 
+                   ] 
+               ) 
+          )
+    elif "autofilter_data" in cb_data:
+          await cmd.message.edit(
+               text=Script.AUTOFILTER_TXT,
+               parse_mode = enums.ParseMode.MARKDOWN, 
+               disable_web_page_preview=True, 
+               reply_markup=InlineKeyboardMarkup(
+                   [                      
+                       [
+                        InlineKeyboardButton("⬇️ Back", callback_data="helpbutton_data"),
+                        InlineKeyboardButton("🔐 Close", callback_data="close_data")
+                       ]
+                   ]
+               )
+          )
+    elif "about_data" in cb_data:
+          await cmd.message.edit(
+               text=Script.ABOUT_TXT,
+               parse_mode = enums.ParseMode.MARKDOWN, 
+               disable_web_page_preview=True, 
+               reply_markup=InlineKeyboardMarkup(
+                   [                      
+                       [
+                        InlineKeyboardButton("⬇️ Back", callback_data="helpbutton_data"),
+                        InlineKeyboardButton("🔐 Close", callback_data="close_data")
+                       ]
+                   ]
+               )
+          )
+
+    elif "back_data" in cb_data:
+          await cmd.message.edit(
+               text=Translation.START_TEXT.format(cmd.from_user.first_name, Config.ADMIN_USERNAME),
+               parse_mode = enums.ParseMode.MARKDOWN, 
+               disable_web_page_preview=True, 
+               reply_markup=InlineKeyboardMarkup(
+                   [
+                      
+                       [
+                        InlineKeyboardButton("Help", callback_data="helpbutton_data"),
+                        InlineKeyboardButton("About", callback_data="about_data")
+                       ],[
+                        InlineKeyboardButton("Developer", url="https://t.me/Lx0980AI")                                               
+                       ]
+                   ]
+               )
+          )
+    elif "close_data" in cb_data:
+          await cmd.message.delete()
+          await cmd.message.reply_to_message.delete()
