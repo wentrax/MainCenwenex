c22b85cd1d2f554115c90ebdccde56f644bbb957
Lx 0980 <93909675+alex0980@users.noreply.github.com>
2023-04-29 23:41:45 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -4,7 +4,7 @@ logging.basicConfig(level=logging.DEBUG,
                     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 logger = logging.getLogger(__name__)
 from pyrogram import filters, enums
-from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery
 from bot import Bot as Cenwenex
 
 @Cenwenex.on_message(filters.command("start") & filters.private)
@@ -30,7 +30,7 @@ async def start(bot, cmd):
 
 
  
-@Robot.on_callback_query()
+@Cenwenex.on_callback_query()
 async def button(bot, cmd: CallbackQuery):
     cb_data = cmd.data
     if "helpbutton_data" in cb_data:
@@ -46,7 +46,8 @@ async def button(bot, cmd: CallbackQuery):
                      ],[
                       InlineKeyboardButton("Developer", url="https://t.me/Lx0980AI")
                      ],[
-                      InlineKeyboardButton("🔐 CLOSE", callback_data="close_data")
+                      InlineKeyboardButton("⬇️ Back", callback_data="start_data")
+                      InlineKeyboardButton("🔐 Close", callback_data="close_data")
                      ]
  
                  ] 
@@ -88,16 +89,16 @@ async def button(bot, cmd: CallbackQuery):
                reply_markup=InlineKeyboardMarkup(
                    [                      
                        [
-                        InlineKeyboardButton("⬇️ Back", callback_data="helpbutton_data"),
+                        InlineKeyboardButton("⬇️ Back", callback_data="start_data"),
                         InlineKeyboardButton("🔐 Close", callback_data="close_data")
                        ]
                    ]
                )
           )
 
-    elif "back_data" in cb_data:
+    elif "start_data" in cb_data:
           await cmd.message.edit(
-               text=Translation.START_TEXT.format(cmd.from_user.first_name, Config.ADMIN_USERNAME),
+               text=Script.START_TXT.format(cmd.from_user.first_name), 
                parse_mode = enums.ParseMode.MARKDOWN, 
                disable_web_page_preview=True, 
                reply_markup=InlineKeyboardMarkup(
