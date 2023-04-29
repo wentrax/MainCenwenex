9ee8e14cf2eec627ee8de52e9f7c76871e74510c
Lx 0980 <93909675+alex0980@users.noreply.github.com>
2023-04-29 22:24:50 +0530

    Update info.py

--- info.py
+++ info.py
@@ -1,1 +1,22 @@
 
+import os
+import logging
+
+class Config:
+    
+    API_ID = 28298577
+    API_HASH = "143efc9236a955d630ede2642578b251"
+    BOT_TOKEN = "5068899226:AAGrSpHXOetSe-q_YSL4Sa6ghs2hhdn2Ls4"
+    BOT_SESSION = "bot" 
+    CAPTION = "@HQFilms4U"
+
+    FROM_CHANNEL = "@Databaseforfiles"
+    TO_CHANNEL = -1001563098453
+
+    OWNER_ID = "1903280447"
+    LIMIT = 2
+    SKIP_NO = 0
+    SESSION = "BQAbcBp7IplE9_BJIw05yKC7WZIoUDTFbnXfDLGyIYFpq76tfLLDtlqcfRyZ3rTBgtRxKrZhvsxh1DVzdhxekQuX73dmodoLM8WapEGu390jUb5vHP4y73RwgbHYFYiilMx_FFa60LHH6K1DGzhlk0dvnVn1X634zHvdpW0nmXYhMwH0nUghqSzgXPIJaE_AVHx2ZzMBv-XVluDZZJJIg2Tr2eCg8SNifCz22WJWGbM3VcGm-7VJt6S4wgoAyntndgJx9jvRvpCuOGBJ1h2YdypVh-KDRwpak_4CHubx1NTvYtDzgDzA4grO0UaO1JWURvBvl5I0zUWe2jC2IeSBgj0qAAAAAWHA4M8A"
+
+def LOGGER(name: str) -> logging.Logger:
+    return logging.getLogger(name)
