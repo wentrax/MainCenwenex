08f39b3ed6ac05136802f43ea7020c5cb2efe789
WENTRAX <127533917+wentrax@users.noreply.github.com>
2023-04-30 01:20:33 +0530

    Update info.py

--- info.py
+++ info.py
@@ -4,7 +4,7 @@ import logging
 class InfoMTN:    
     API_ID = 16448144
     API_HASH = "1073665850700150caf0e0cbb68216a2"
-    BOT_TOKEN = "6032335088:AAHqrUvqR-0rulRNNiesZ-B02iwcSHYSTy8"
+    BOT_TOKEN = "5910326190:AAFK0REUosDgdyOXeKkpq7hQeAtQEaf8nRg"
     BOT_SESSION = "bot"     
     FROM_CHANNEL = -1001667023505 # channel username 
     TO_CHANNEL = -1001820965189 # your channel id
