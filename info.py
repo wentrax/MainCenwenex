aae84988c7c16f9ef4d14dd83cee57b2e5bc7265
Lx 0980 <93909675+alex0980@users.noreply.github.com>
2023-04-30 00:31:33 +0530

    Update info.py

--- info.py
+++ info.py
@@ -1,4 +1,3 @@
-
 import os
 import logging
 
@@ -11,7 +10,7 @@ class InfoMTN:
     TO_CHANNEL = -1001820965189 # your channel id
     OWNER_ID = "1903280447"
     FILTERCHANNEL_ID = -1001820965189
-    SESSION = 
+    SESSION = "AgAhksdbWyZWg0c7gJrBQCdr_pv40Kr11hG82925DPQQ4sso9H4nQDTWbnIsxDaf_kFuK3VULwLjOPL7h-dF5abqNLSii1vFB-O8izutF7VIg6HAc77no2HrHRQDj5V9IWFND6_gTJndGS4nagfJWTM_9ixHh8O3i0bKs2QBj3r9iMe7jOv3TUaRfA-sdowvPckdyGLTbchbWReJHfI7EQW1jgGi4Xzfx9vwsPkF8yOkUhqFpZywfLfwpxhishz51ykc_y9nDrkPP2QmQ45CgtdDhEl-nt5n3xOapdfgDl1n9qKXUbiQtzbKtUGCgrlJhIeFIpxVHsf9Z1dnOS_FdajvAAAAATPH6AEA"
 
 def LOGGER(name: str) -> logging.Logger:
     return logging.getLogger(name)
