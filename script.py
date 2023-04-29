35f780bf70114b403b911e0f3f2e48b6708d7b21
Lx 0980 <93909675+alex0980@users.noreply.github.com>
2023-04-29 22:42:18 +0530

    Update script.py

--- script.py
+++ script.py
@@ -1,1 +1,36 @@
+class CenwenexTXT(object):  
+  START_TXT = """
+Hello there, I am <b>Cenwenex</b>
+"""
+  HELP_TXT = """
+Help: <b>Buttons</b> 
+"""
 
+AUTOFILTER_TXT = """
+<b>Bot simply search for the files from provided channel according to given query and gives link to those files as buttons!</b>
+
+<b>How to use the bot??</b>
+<i>    
+* Add user account to your channel !
+* Add bot to group with admin rights.
+* Once deployed and bot is started, send a message to channel (must do step)
+* Bot will work now :)
+No need add each filter again!
+Bot will automatically search for your file and give links to that!
+</i>
+"""
+
+MEDIACLONE_TXT = """
+<b>This Bot forward all files from One Public channel to Your Personal channel</b>
+
+<b>Follow These Steps!!</b>
+<b>• Currectly fill your Heroku Config vars</b> <code>FROM_CHANNEL</code> and <code>TO_CHANNEL</code> <b>and other Vars</b>
+<b>• Then give admin permission in your personal telegram channel</b>
+<b>• Then send any message In your personal telegram channel</b>
+<b>• Then use /clone command in your bot</b>
+<b><u>Available Command</b></u>
+* /clone - <b>start forward</b>
+
+<b> @Lx0980AI</b>
+"""
+  
